c0506bce9e73e681881b80b603a164b680641489
zhangxinxu <zhangxinxu@zhangxinxu.com>
2014-01-19 18:46:24 +0800

    meta
    meta change

--- demo/_include/header.php
+++ demo/_include/header.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="description" content="Snap.svg学习实例页面之<?php echo $key; ?> » 张鑫旭-鑫空间-鑫生活" />
-<meta name="description" content="Snap.svg学习实例页面之<?php echo $key; ?>" />
-<meta name="keywords" content="Snap.svg, demo, SVG" />
+<meta name="description" content="Snap.svg中文API文档兼学习实例页面之<?php echo $key; ?>" />
+<meta name="keywords" content="Snap.svg, demo, SV, API, 中文, 文档" />
 <meta name="author" content="张鑫旭, zhangxinxu" />
-<title>Snap.svg学习实例页面之<?php echo $key; ?> » 张鑫旭-鑫空间-鑫生活</title>
+<title>Snap.svg中文API文档兼学习实例页面之<?php echo $key; ?> » 张鑫旭-鑫空间-鑫生活</title>
 <link rel="stylesheet" href="../../static/demo.css" />
