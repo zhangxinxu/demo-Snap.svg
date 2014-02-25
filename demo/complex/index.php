987541fc089b538d9e6fc9557d9d55ce57b33b16
zhangxinxu <zhangxinxu@zhangxinxu.com>
2014-02-25 11:48:17 +0800

    add a complex demo link
    add a complex demo link

--- demo/complex/index.php
+++ demo/complex/index.php
@@ -13,7 +13,16 @@
 ?>
 
 <div id="zxxBody" class="zxx_body">
-	<div class="zxx_api_content"><p>稍等...您可以先观摩官方提供的几个复杂demo.</p></div>
+	<div class="zxx_api_content">
+    	<div class="zxx_api_compl_x">
+        	<a href="http://www.zhangxinxu.com/sp/fish/" target="_blank" class="zxx_api_compl_li">
+            	<div class="zxx_api_compl_img">
+                	<img src="../../static/pic/compl-demo-1.png">
+                </div>
+                <div>钓鱼知识入门</div>
+            </a>
+        </div>
+    </div>
 </div>
 
 <?php 
