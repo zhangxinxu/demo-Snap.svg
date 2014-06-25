bcc626547e90274e0e2b18d2e38ecbd20a246bb8
zhangxinxu <zhangxinxu@zhangxinxu.com>
2014-06-25 09:50:17 +0800

    add two complex demos
    add two complex demos

--- demo/complex/index.php
+++ demo/complex/index.php
@@ -21,6 +21,18 @@
                 </div>
                 <div>钓鱼知识入门</div>
             </a>
+            <a href="http://www.zhangxinxu.com/study/201406/svg-animation-img-hover-effect.html" target="_blank" class="zxx_api_compl_li">
+            	<div class="zxx_api_compl_img">
+                	<img src="../../static/pic/compl-demo-2.gif" alt="折角翻页效果">
+                </div>
+                <div>折角翻页效果</div>
+            </a>
+            <a href="http://www.zhangxinxu.com/study/201406/svg-animation-pandora-improve.html" target="_blank" class="zxx_api_compl_li">
+            	<div class="zxx_api_compl_img">
+                	<img src="../../static/pic/compl-demo-3.gif" alt="潘多拉效果">
+                </div>
+                <div>潘多拉效果</div>
+            </a>
         </div>
     </div>
 </div>
